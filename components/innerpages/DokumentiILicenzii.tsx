efbbd0f800c9ef48a833740c1ee0b384c744b518
kirill Iluschenko <kirill.i@nekidaem.ru>
2022-08-24 23:38:44 +0300

    [licenses] - CTD-365  - added new license files

--- components/innerpages/DokumentiILicenzii.tsx
+++ components/innerpages/DokumentiILicenzii.tsx
@@ -3,19 +3,32 @@ const DokumentiILicenzii = () => (
     <h1>Документы и лицензии</h1>
 
     <h2 className="custom-title">Лицензия</h2>
-    <div className="license__block">
+    {/* <div className="license__block">
       <img className="license__image" src="/images/licenses/license_01.webp" />
       <img className="license__image" src="/images/licenses/license_02.webp" />
       <img className="license__image" src="/images/licenses/license_03.webp" />
-    </div>
+    </div> */}
     <div className="mt-2 ">
+      <div>
       <a
-        href="/pdfs/Licenziya.pdf"
+        href="/pdfs/licenses_26_07_2022.pdf"
+        className="custom-text-link mr-4"
+        target="_blank"
+        title="Выписка из реестра лицензий по состоянию на 26.07.2022г."
+      >
+        Выписка из реестра лицензий по состоянию на 26.07.2022г (PDF)
+      </a>
+      </div>
+      <div>
+      <a
+        href="/pdfs/licenses_08_06_2022.pdf"
         className="custom-text-link"
         target="_blank"
+        title="Выписка из реестра лицензий по состоянию на 08.06.2022г."
       >
-        Открыть в формате PDF
+        Выписка из реестра лицензий по состоянию на 08.06.2022г (PDF)
       </a>
+      </div>
       <p className="custom-text">
         Обращаем Ваше внимание, что с января 2021 года лицензии на бланках не
         выдаются, а учет осуществляемых видов деятельности реализуется путем
