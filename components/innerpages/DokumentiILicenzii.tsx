719d5cfb2da0c263b8699b2ec563a46788b813f5
K.Iluschenko <iluschenko@artw.ru>
2024-05-30 21:36:23 +0300

    added new documents

--- components/innerpages/DokumentiILicenzii.tsx
+++ components/innerpages/DokumentiILicenzii.tsx
@@ -47,10 +47,10 @@ const DokumentiILicenzii = () => (
     </div>
 
     <h2 className="custom-title">Санитарно-эпидемиологическое заключение</h2>
-    <div className="license__block">
-      <img className="license__image" src="/images/licenses/sez_01.webp" />
-      <img className="license__image" src="/images/licenses/sez_02.webp" />
-    </div>
+    {/*<div className="license__block">*/}
+    {/*  <img className="license__image" src="/images/licenses/sez_01.webp" />*/}
+    {/*  <img className="license__image" src="/images/licenses/sez_02.webp" />*/}
+    {/*</div>*/}
     <div className="mt-2 ">
       <a href="/pdfs/SEZ.pdf" className="custom-text-link" target="_blank">
         Открыть в формате PDF
